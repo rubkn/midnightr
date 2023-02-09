9bf27beacd0f8cd529511c7ce233b14b8d5487c6
rubenbrandao <rubenavbrandao@gmail.com>
2023-02-09 00:14:17 +0000

    chore: removed i18next support temporarily

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,8 @@
 import '@styles/globals.css';
-import i18n from '../next-i18next.config';
 import { SessionProvider } from 'next-auth/react';
 
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
-import { appWithTranslation } from 'next-i18next';
 
 const MyApp = ({
   Component,
@@ -19,4 +17,4 @@ const MyApp = ({
   );
 };
 
-export default appWithTranslation(MyApp, i18n);
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import { Suspense } from 'react';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { useTranslation } from 'next-i18next';
 import { type NextPage } from 'next';
 
 import Wrapper from '@layouts/Wrapper';
@@ -8,7 +6,6 @@ import { useSession } from 'next-auth/react';
 import Login from '@components/Login';
 
 const Home: NextPage = () => {
-  const { t } = useTranslation('common');
   const { data: session } = useSession();
 
   if (!session) {
@@ -21,13 +18,5 @@ const Home: NextPage = () => {
     </Suspense>
   );
 };
-
-export async function getStaticProps({ locale = 'en-gb' }) {
-  return {
-    props: {
-      ...(await serverSideTranslations(locale, ['common']))
-    }
-  };
-}
 
 export default Home;
