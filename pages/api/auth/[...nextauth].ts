b3362d2569b42f7e06a72568686005d0568ae9b8
rubenbrandao <rubenavbrandao@gmail.com>
2023-02-16 15:17:36 +0000

    fix: auth account linking

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,19 +4,20 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import prisma from '@lib/prisma';
 
 export const authOptions: NextAuthOptions = {
-  callbacks: {
+  /* callbacks: {
     session({ session, user }) {
       if (session.user) {
         session.user.id = user.id;
       }
       return session;
     }
-  },
+  }, */
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      allowDangerousEmailAccountLinking: true
     })
   ],
   secret: process.env.NEXTAUTH_SECRET
