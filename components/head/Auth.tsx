a7e764c9721c4e81d83df9cb5802349d9b401f6f
rubenbrandao <rubenavbrandao@gmail.com>
2023-12-07 12:40:53 +0000

    chore: improvements

--- components/head/Auth.tsx
+++ components/head/Auth.tsx
@@ -8,19 +8,7 @@ const Auth: FC = () => {
 
   return (
     <>
-      {!session?.user && (
-        <div>
-          <button
-            className="rounded-lg border px-4 py-2 text-sm text-white transition-all hover:border-violet hover:bg-gradient-to-r hover:from-violet hover:to-dark-violet"
-            onClick={() => {
-              signIn('google');
-            }}
-          >
-            Sign in
-          </button>
-        </div>
-      )}
-      {session?.user && (
+      {session?.user ? (
         <div className="flex items-center space-x-4">
           {/* <h1>Logged in as {session?.user?.email}</h1> */}
           <button
@@ -44,6 +32,17 @@ const Auth: FC = () => {
               alt={`${session.user?.name} Avatar`}
             />
           </Link>
+        </div>
+      ) : (
+        <div>
+          <button
+            className="rounded-lg border px-4 py-2 text-sm text-white transition-all hover:border-violet hover:bg-gradient-to-r hover:from-violet hover:to-dark-violet"
+            onClick={() => {
+              signIn('google');
+            }}
+          >
+            Sign in
+          </button>
         </div>
       )}
     </>
